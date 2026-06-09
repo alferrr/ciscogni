5606843341c559165c0d71bb87a230df2be310d4
Alfer Mercado <alfzmercado240@gmail.com>
2026-06-09 13:10:58 +0800

    fix: removed corrupted /frontend folder

--- frontend/.next/dev/static/chunks/src_app_page_tsx_0f5y3d7._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/src_02wfu18._.css","static/chunks/node_modules_0rvt5lg._.js","static/chunks/_0pt.pf_._.js"],
-    source: "dynamic"
-});
